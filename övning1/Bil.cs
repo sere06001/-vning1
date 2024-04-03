5da4d8730e1a2716aa7348bed9ff5cbb58973816
sere06001 <sere06001@utb.vaxjo.se>
2024-04-03 10:29:07 +0200

    Add project files.

---
+++ övning1/Bil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace övning1
+{
+    internal class Bil
+    {
+        public string märke;
+        public string modell;
+        public int årsmodell;
+        public int streckakörd;
+        public string plåtnummer;
+        public Bil(string mä, string mo, int år, int str, string plåt)
+        {
+            märke = mä;
+            modell = mo;
+            årsmodell = år;
+            streckakörd = str;
+            plåtnummer = plåt;
+        }
+        public Bil() { }
+    }
+}
---
+++ övning1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace övning1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bil bil1 = new Bil("volvo", "XC90", 2016, 170000, "ABC123");
+            Console.WriteLine($"Märke: {bil1.märke}");
+            Console.WriteLine($"Modell: {bil1.modell}");
+            Console.WriteLine($"Årsmodell: {bil1.årsmodell}");
+            Console.WriteLine($"Strecka körd: {bil1.streckakörd}");
+            Console.WriteLine($"Nummerplåt: {bil1.plåtnummer}");
+        }
+    }
+}
